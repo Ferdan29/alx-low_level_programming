4345a775aeb4c4a4076ae8d91728a86973e49c71
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-09 20:29:51 -0700

    huty

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Ogunjale Feranmi Daniel
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char casez;
+
+	for (casez = 'a'; casez <= 'z'; cesez++)
+		putchar(casez);
+
+	putchar('\n');
+
+	return (0);
+}
