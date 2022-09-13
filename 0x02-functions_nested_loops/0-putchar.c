e3d5a8ff6714593bf20347b00c952d70bbe57480
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-13 14:28:28 -0700

    gone

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Ogunjale Feranmi
+ */
+
+#include <stdio.h>
+
+/**
+ * main - print out a given output.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
+
