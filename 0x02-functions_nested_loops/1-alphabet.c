350b87d69de6a3eac1226b025b84f701731357de
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-13 15:28:57 -0700

    gone

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Ogunjale Feranmi
+ */
+
+#include <stdio.h>
+
+/**
+ * main - print all alphabet from a to z in lower case
+ *
+ *Return: 0.
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	int big = void print_alphabet();
+
+	return 0;
+}
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
