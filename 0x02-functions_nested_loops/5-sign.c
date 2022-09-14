b088ab08366e6257eb603fa9d38f05b01cec3147
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-14 07:30:56 -0700

    gone

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -18,15 +18,13 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else if (n < 0)
-	{
-		_putchar('0');
-		return (-1);
-	}
 	else
 	{
-
+		if (n < 0)
+		{
+			_putchar('0');
+			return (-1);
+		}
 	}
 	_putchar('\n');
 }
-
