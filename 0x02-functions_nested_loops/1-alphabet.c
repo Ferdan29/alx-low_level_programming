a8879f265c7e8755b87cb587c8fcea7d1a0853b0
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-13 23:03:26 -0700

    gone

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
  * main - print all alphabet from a to z in lower case
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Ogunjale Feranmi
+ * Desc: Header file containing declaratino for all function
+ * used in the 2x02-functions_nested_loop directory.
+ * 
+ */
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif
