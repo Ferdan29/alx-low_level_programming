9ca28239b1c9ceb406f7586d0c9f8fffb1860b81
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-14 08:52:56 -0700

    gone

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - function that print the absolute value of an integer
+ *@n: The character to be checked.
+ * Return:0 and 1
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
