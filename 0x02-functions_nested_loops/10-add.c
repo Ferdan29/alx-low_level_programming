b1f6406c6f09ca14e64da4a5b6d75e0033122de7
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-14 10:50:59 -0700

    gone

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - function to add to numbers
+ * @re: the character to check
+ *
+ * Return: the result
+ */
+int add(int k, int y)
+{
+	int re;
+
+	re = k + y;
+
+	_putchar(re);
+
+	return (re);
+}
+
