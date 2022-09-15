79961e2d7bb88f7d4ee99b3514f23fb07ac4174f
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-15 06:02:04 -0700

    cool

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that print a sequence of number
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i);
+	_putchar('\n');
+}
