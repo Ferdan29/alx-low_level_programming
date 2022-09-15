2feeaf3bfac22abff7eef80635aab9a8935052a2
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-14 17:23:44 -0700

    gone

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_times_table - function that print time table
+ * @n: the variable to be checked
+ *
+ * Return: Always 0.
+ */
+void print_times_table(int n)
+{
+	int k, j, p;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (k = 0; k <= n; k++)
+		{
+			_putchar('0');
+		
+			for (j = 0; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				p = k * j;
+
+				if (p < 99)
+					_putchar(' ');
+				if (p < 9)
+					_putchar(' ');
+
+				if (p > 100)
+				{
+					_putchar((p / 100) + '0');
+					-putchar(((p / 10) % 10) + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');			
+			}
+			_putchar('\n');
+		}
+	}
+}
+
