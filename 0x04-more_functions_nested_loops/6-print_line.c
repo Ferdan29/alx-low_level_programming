74fb12387508039d1dbb90261bc774b35cffecbf
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-15 17:24:11 -0700

    cool

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line
+ * @n: variable to be read
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int j = 0;
+
+	if (n > 0)
+	{
+		while (j <= n)
+		{
+			_putchar('_');
+			j++;
+		}
+	}
+	_putchar('\n');
+}
