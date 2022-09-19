c5a2b3a0ed8d45ee3254e19a145d24cb88e4a816
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-19 14:51:50 -0700

    great

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,9 @@
 
 /**
  * reset_to_98 - function that takes a pointer to an int
- * 	as parameter and updates the value
+ * as parameter and updates the value
  * @*n: pointer variable to be checked
+ * @n: variable to be checked
  *
  * Return: Always 0.
  */
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-ifndef MAIN_H
-define MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
  * File: main.h
@@ -20,4 +20,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-endif
+#endif
