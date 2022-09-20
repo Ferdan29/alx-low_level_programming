5bc49644d99985135163f5550642b4d18c14dce6
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-19 18:59:24 -0700

    great

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string
+ * @s: pointer variable
+ * 
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int k;
+
+	for (k = 0; s[k] != 0; k++)
+	{
+	}
+	for (k = k - 1; k >= 0; k--)
+		_putchar(s[k]);
+	_putchar('\n');
+}
