24bb5d92a1a26850aef4df21a589ec97d34097a6
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-21 16:38:53 -0700

    task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: variable to be checked
+ * @src: variable to be checked
+ *
+ * Return: pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int j, k;
+
+	for (j = 0; dest[j] != '\0'; j++)
+		dest[j];
+	for (k = 0; src[k] != '\0'; k++
+	{
+		dest[j] = src[k];
+		j++;
+	}
+
+	dest[j] = '\0';
+	return (dest);
+
+
+}
