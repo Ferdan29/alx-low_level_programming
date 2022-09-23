de7873349eae3da35277de886e69be4f0a1a0dde
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-22 22:04:33 -0700

    task

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -20,13 +20,14 @@ char *rot13(char *str)
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 				'T', 'U', 'V', 'W', 'X', 'Y',
 				'Z', 'A', 'B', 'C', 'D', 'E',
-				'F', 'G', 'H', 'I', 'J', 'K'
+				'F', 'G', 'H', 'I', 'J', 'K',
 				'L', 'M', 'n', 'o', 'p', 'q',
 				'r', 's', 't', 'u', 'v', 'w',
 				'x', 'y', 'z', 'a', 'b', 'c',
 				'd', 'e', 'f', 'g', 'h', 'i',
 				'j', 'k', 'l', 'm'};
-	
+
+
 	while (str[indx1])
 	{
 		for (indx2 = 0; indx2 < 52; indx2++)
@@ -40,4 +41,4 @@ char *rot13(char *str)
 		indx1++;
 	}
 	return (str);
-},
+}
