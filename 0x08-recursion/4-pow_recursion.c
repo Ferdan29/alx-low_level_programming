429a3b42232cd7fa3a3d7f9f4c953895028c24b8
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-29 19:45:38 -0700

    task

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/** * _pow_recursion - function that returns the value of x ^ y
+ * @x: variable
+ * @y: variable
+ *
+ * Return: -1 and result
+ */
+int _pow_recursion(int x, int y)
+{
+	int j = 0, k = x;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (j < y)
+	{
+		k *= y;
+		_pow_recursion(x, y);
+		j++;
+	}
+	return (k);
+}
