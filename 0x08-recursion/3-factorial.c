f5756d0061f198ef0cad1543c14819c3e5ed243a
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-09-29 19:21:27 -0700

    task

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,7 +9,7 @@
 int factorial(int n)
 {
 	int res;
-	
+
 	if (n < 0)
 	{
 		return (-1);
@@ -19,7 +19,7 @@ int factorial(int n)
 		return (1);
 	}
 	else
-		res = n * factorial(n-1);
+		res = n * factorial(n - 1);
 
 	return (res);
 }
