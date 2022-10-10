864c178703206e48b6cff3b4ec6e991d13d2448e
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-10-10 07:28:25 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * struct dog - a struct that takes the details about a dog
+ * @name: variable thay takes the name of the dog
+ * @age: variable that takes the age of the dog
+ * @owner: variable that takes the name of the owner of the dog
+ *
+ * Return: nothing
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
