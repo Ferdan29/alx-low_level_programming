8dd25081e361e60c90248287325611fcd2f0039a
Ferdan29 <ogunjaleferanmi@gmail.com>
2022-10-21 06:28:13 -0700

    list

--- 0x12-singly_linked_lists/_putchar.c
+++ 0x12-singly_linked_lists/_putchar.c
@@ -1,1 +1,14 @@
+#include "lists.h"
+#include <unistd.h>
 
+/**
+ * putchar - function that print out a value
+ * @c: char variable to be checked
+ *
+ * Return: 1 on sucess and -1 on fail.
+ */
+
+int _putchar(char c)
+{
+	return (1, &c, 1);
+}
